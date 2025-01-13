4553a16bce201ebbbffc596bb87a5a2024ba0ab4
Yuri Araujo <yu.dev@icloud.com>
2025-01-13 00:44:39 +0000

    Implements websocket

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from repository.database import db
 from models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 
 @app.route('/payments/pix', methods=['POST'])
@@ -53,9 +55,15 @@ def confirmation_pix():
 def payment_pix_page(payment_id):
   payment = Payment.query.get(payment_id)
 
-  return render_template('payment.html', payment_id=payment.id, value=payment.value, 
-                         host= "http://127.0.0.1:5000", qr_code=payment.qr_code)
+  return render_template('payment.html', 
+                         payment_id=payment.id, 
+                         value=payment.value, 
+                         host= "http://127.0.0.1:5000", 
+                         qr_code=payment.qr_code)
 
+@socketio.on('connect')
+def handle_connect():
+  print('connected')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  socketio.run(app, debug=True)
